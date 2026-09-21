e052848e37c5b6237e0bf977bb8636405d151e72
Mrkwamu <jeffreyfestus1@gmail.com>
2026-09-21 11:44:51 +0100

    initial resolve structure

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+
+import { BrowserModule } from './browser/browser.module';
+
+@Module({
+  imports: [BrowserModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
